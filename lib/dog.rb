6661bd5352d4c567897750087e7d1e207ca67ae2
Margot Konig <margot.konig@blackrock.com>
2019-06-17 23:46:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -67,7 +67,10 @@ class Dog
   def self.new_from_db 
   end 
   
-  def self.find_by_name 
+  def self.find_by_name(name)
+    sql = "SELECT * FROM dogs WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Dog.new(id: result[0], name: result[1], breed: result[2])
   end 
   
   def update

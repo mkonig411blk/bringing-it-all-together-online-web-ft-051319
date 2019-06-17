2b60ee41bcfaeb533eefa08d6629926e46937142
Margot Konig <margot.konig@blackrock.com>
2019-06-17 23:25:43 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,49 @@
+class Dog 
+  attr_accessor :id, :name, :breed 
+  
+  def initialize(id: nil, name:, breed:)
+    @name = name
+    @id = id 
+    @breed = breed 
+  end 
+  
+   def self.drop_table
+    sql =  <<-SQL 
+      DROP TABLE IF EXISTS dogs
+        SQL
+    DB[:conn].execute(sql) 
+  end
+  
+  def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        breed TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
+  end
+  
+  def save 
+  end
+  
+  def self.create 
+  end 
+  
+  def self.find_by_id 
+  end 
+  
+  def self.find_or_create_by
+  end 
+  
+  def self.new_from_db 
+  end 
+  
+  def self.find_by_name 
+  end 
+  
+  def update 
+  end 
+  
+end 

1f42fc0689952b2888a59781d1290f9e88963602
Margot Konig <margot.konig@blackrock.com>
2019-06-17 23:35:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -41,10 +41,16 @@ class Dog
     end
   end
   
-  def self.create 
+  def self.create(name:, breed:)
+    dog = Dog.new(name, breed)
+    dog.save
+    dog
   end 
   
-  def self.find_by_id 
+  def self.find_by_id(id)
+    sql = "SELECT * FROM dogs WHERE id = ?"
+    result = DB[:conn].execute(sql, id)[0]
+    Dog.new(result[0], result[1], result[2])
   end 
   
   def self.find_or_create_by
@@ -56,7 +62,10 @@ class Dog
   def self.find_by_name 
   end 
   
-  def update 
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
   end 
+  
   
 end 
